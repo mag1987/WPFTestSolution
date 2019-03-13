24838505075fded74a8fef40a0529afbef15ee82
mag987 <mag987@list.ru>
2019-03-13 23:04:20 +0700

    prism

--- WpfTest/App.xaml.cs
+++ WpfTest/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.ComponentModel;
 
 namespace WpfTest
 {
@@ -13,5 +14,29 @@ namespace WpfTest
     /// </summary>
     public partial class App : Application
     {
+
+    }
+    public class MyMathModel : BindableBase
+    {
+        private readonly ObservableCollection<int> _myValues = new ObservableCollection<int>();
+        public readonly ReadOnlyObservableCollection<int> MyPublicValues;
+        public MyMathModel()
+        {
+            MyPublicValues = new ReadOnlyObservableCollection<int>(_myValues);
+        }
+        //добавление в коллекцию числа и уведомление об изменении суммы
+        public void AddValue(int value)
+        {
+            _myValues.Add(value);
+            RaisePropertyChanged("Sum");
+        }
+        //проверка на валидность, удаление из коллекции и уведомление об изменении суммы
+        public void RemoveValue(int index)
+        {
+            //проверка на валидность удаления из коллекции - обязанность модели
+            if (index >= 0 && index < _myValues.Count) _myValues.RemoveAt(index);
+            RaisePropertyChanged("Sum");
+        }
+        public int Sum => MyPublicValues.Sum(); //сумма
     }
 }
