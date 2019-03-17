21709d2788f2e25a041b4bfb83843df40b35c556
mag987 <mag987@list.ru>
2019-03-17 22:43:23 +0700

    Binding successfull

--- WpfTest/App.xaml.cs
+++ WpfTest/App.xaml.cs
@@ -6,37 +6,92 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.ComponentModel;
+using Prism.Commands;
 
 namespace WpfTest
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, INotifyPropertyChanged
     {
+        private readonly List<int> _myValues = new List<int>();
+        public readonly List<int> MyPublicValues;
 
-    }
-    public class MyMathModel : BindableBase
-    {
-        private readonly ObservableCollection<int> _myValues = new ObservableCollection<int>();
-        public readonly ReadOnlyObservableCollection<int> MyPublicValues;
-        public MyMathModel()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
         {
-            MyPublicValues = new ReadOnlyObservableCollection<int>(_myValues);
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public App()
+        {
+            MyPublicValues = new List<int>(_myValues);
         }
         //добавление в коллекцию числа и уведомление об изменении суммы
         public void AddValue(int value)
         {
             _myValues.Add(value);
-            RaisePropertyChanged("Sum");
+            OnPropertyChanged("Sum");
         }
         //проверка на валидность, удаление из коллекции и уведомление об изменении суммы
         public void RemoveValue(int index)
         {
             //проверка на валидность удаления из коллекции - обязанность модели
             if (index >= 0 && index < _myValues.Count) _myValues.RemoveAt(index);
-            RaisePropertyChanged("Sum");
+            OnPropertyChanged("Sum");
         }
         public int Sum => MyPublicValues.Sum(); //сумма
     }
+    public class MainVM : INotifyPropertyChanged
+    {
+        readonly App _model = new App();
+        public MainVM()
+        {
+            //таким нехитрым способом мы пробрасываем изменившиеся свойства модели во View
+            _model.PropertyChanged += (s, e) => { OnPropertyChanged(e.PropertyName); };
+            AddCommand = new DelegateCommand<string>(str => {
+                //проверка на валидность ввода - обязанность VM
+                int ival;
+                if (int.TryParse(str, out ival)) _model.AddValue(ival);
+            });
+            RemoveCommand = new DelegateCommand<int?>(i => {
+                if (i.HasValue) _model.RemoveValue(i.Value);
+            });
+        }
+        public DelegateCommand<string> AddCommand { get; }
+        public DelegateCommand<int?> RemoveCommand { get; }
+        public int Sum => _model.Sum;
+        public List<int> MyValues => _model.MyPublicValues;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+    public class TestVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public DelegateCommand TestDelegateCommand
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    MessageBox.Show("Hallo");
+                });
+            }
+        }
+          
+    }
+
 }
---
+++ WpfTest/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+
+namespace WpfTest
+{
+    class ViewModel
+    {
+        public delegate int testDelgate();
+        public DelegateCommand<string> AddCommand { get; }
+    }
+}
