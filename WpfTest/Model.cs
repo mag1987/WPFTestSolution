582be5226975a636287fe5aa20bd059a68f18c95
mag987 <mag987@list.ru>
2019-04-03 16:39:23 +0700

    clarified

--- WpfTest/Model.cs
+++ WpfTest/Model.cs
@@ -15,42 +15,28 @@ namespace WpfTest
 {
     public class Model : BindableBase
     {
+        /*
         private readonly ObservableCollection<int> _myValues = new ObservableCollection<int>();
         public readonly ReadOnlyObservableCollection<int> MyPublicValues;
+    */    
         public Model()
         {
-            MyPublicValues = new ReadOnlyObservableCollection<int>(_myValues);
-            _chemShifts = new ObservableCollection<ChemShift>()
+            ChemShifts = new ObservableCollection<ChemShift>()
             {
                 new ChemShift(1.2,"ppm","sd" ),
                 new ChemShift(2.0,"ppm","rer" ),
                 new ChemShift(3.0,"ppm","wewe" )
             };
-            ChemShifts = new ReadOnlyObservableCollection<ChemShift>(_chemShifts);
-
-            ChemShiftsPublic = new ChemShifts();
-            ChemShiftsPublic.AddProperty( new ChemShiftProperty("shift", 2.3));
-            ChemShiftsPublic.AddProperty(new ChemShiftProperty("beta", 3));
-            ChemShiftsPublic.AddShift();
-            ChemShiftsPublic.AddShift();
         }
-        //добавление в коллекцию числа и уведомление об изменении суммы
-        public void AddValue(int value)
-        {
-            _myValues.Add(value);
-            RaisePropertyChanged("Sum");
-        }
-        //проверка на валидность, удаление из коллекции и уведомление об изменении суммы
+        /*
         public void RemoveValue(int index)
         {
             //проверка на валидность удаления из коллекции - обязанность модели
             if (index >= 0 && index < _myValues.Count) _myValues.RemoveAt(index);
             RaisePropertyChanged("Sum");
         }
-        public int Sum => MyPublicValues.Sum(); //сумма
-        private readonly ObservableCollection<ChemShift> _chemShifts = new ObservableCollection<ChemShift>();
-        public readonly ReadOnlyObservableCollection<ChemShift> ChemShifts;
-        public ChemShifts ChemShiftsPublic { get; set; }
+        */
+        public ObservableCollection<ChemShift> ChemShifts { get; set; }
     }
     public class ChemShift
     {
@@ -65,71 +51,6 @@ namespace WpfTest
             Unit = unit;
             Assignment = assignment;
         }
-    }
-    public class ChemShiftProperty
-    {
-        public string Name { get; set; }
-        public Object Value { get; set; }
-        public ChemShiftProperty()
-        {
-            Name = "NewProperty";
-            Value = null;
-        }
-        public ChemShiftProperty(string name, Object value)
-        {
-            Name = name;
-            Value = value;
-        }
-        public ChemShiftProperty(string name) : this(name , null) { }
-    }
-    public class ChemShift1
-    {
-        public List<ChemShiftProperty> Properties { get; set; }
-        public ChemShift1()
-        {
-            Properties = new List<ChemShiftProperty>();
-        }
-        public ChemShift1(List<ChemShiftProperty> properties)
-        {
-            Properties = new List<ChemShiftProperty>(properties);
-        }
-    }
-    public class ChemShifts : ObservableCollection<ChemShift1>
-    {
-        private List<ChemShift1> _shifts { get; set; }
-        private List<ChemShiftProperty> _shiftProperties { get; set; }
-
-        public List<ChemShift1> Shifts => _shifts;
-        public ChemShifts()
-        {
-            _shifts = new List<ChemShift1>();
-            _shiftProperties = new List<ChemShiftProperty>();
-        }
-        public void AddProperty(ChemShiftProperty newProperty)
-        {
-            _shiftProperties.Add(newProperty);
-            foreach (var shift in _shifts)
-            {
-                shift.Properties.Add(newProperty);
-            }
-        }
-        public void DeleteProperty(ChemShiftProperty propertyToDelete)
-        {
-            _shiftProperties.Remove(propertyToDelete);
-            foreach (var shift in _shifts)
-            {
-                shift.Properties.Remove(propertyToDelete);
-            }
-        }
-        public void AddShift()
-        {
-            _shifts.Add(new ChemShift1(_shiftProperties));
-        }
-        public void DeleteShift(ChemShift1 shiftToDelete)
-        {
-            _shifts.Remove(shiftToDelete);
-        }
-
     }
     
     
--- WpfTest/ViewModel.cs
+++ WpfTest/ViewModel.cs
@@ -15,50 +15,17 @@ namespace WpfTest
 {
     public class TestVM : BindableBase
     {
-        public DelegateCommand TestDelegateCommand
-        {
-            get
-            {
-                return new DelegateCommand(() =>
-                {
-                    MessageBox.Show("Hallo from file");
-                });
-            }
-        }
-       
         readonly Model _model = new Model();
-        public DelegateCommand<string> AddCommand { get; }
-        public DelegateCommand<int?> RemoveCommand { get; }
-        public int Sum => _model.Sum;
-        public ReadOnlyObservableCollection<int> MyValues => _model.MyPublicValues;
-        public ReadOnlyObservableCollection<ChemShift> testGridSource => _model.ChemShifts;
-        public ChemShifts ChemShiftsPublicVM => _model.ChemShiftsPublic;
-
+        //public DelegateCommand<string> AddCommand { get; }
+        public ObservableCollection<ChemShift> testGridSource => _model.ChemShifts;
 
         public DelegateCommand<DataGrid> AddNewColumn { get; }
         public DelegateCommand<DataGrid> DeleteColumn { get; }
         public DelegateCommand<DataGrid> GetData { get; }
 
-        public Binding binding { get; set; }
-        public void testMethod()
-        {
-            binding = new Binding();
-            binding.Source = _model.ChemShiftsPublic;
-            
-        }
-        
         public TestVM()
         {
-            //таким нехитрым способом мы пробрасываем изменившиеся свойства модели во View
             _model.PropertyChanged += (s, e) => { RaisePropertyChanged(e.PropertyName); };
-            AddCommand = new DelegateCommand<string>(str => {
-                //проверка на валидность ввода - обязанность VM
-                int ival;
-                if (int.TryParse(str, out ival)) _model.AddValue(ival);
-            });
-            RemoveCommand = new DelegateCommand<int?>(i => {
-                if (i.HasValue) _model.RemoveValue(i.Value);
-            });
             AddNewColumn = new DelegateCommand<DataGrid>(datagrid => {
                 DataGridTextColumn cnew = new DataGridTextColumn();
                 cnew.Header = "Some column";
@@ -68,15 +35,6 @@ namespace WpfTest
             DeleteColumn = new DelegateCommand<DataGrid>(datagrid=> {
                 if (datagrid.SelectedCells.Count == 1)
                     datagrid.Columns.Remove(datagrid.SelectedCells.First().Column);
-            });
-            GetData = new DelegateCommand<DataGrid>(datagrid =>{
-                datagrid.Columns.Add(
-                    new DataGridTextColumn
-                    {
-                        Header = "New column 1",
-                        Width = 50
-                    });
-                
             });
         }
     }
